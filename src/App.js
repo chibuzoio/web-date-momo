475c58e97316f143de7a4e64d5ad84ec6cf40880
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2022-12-14 00:34:20 +0200

    Beginnig to develop the web pages part of DateMomo application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    /* Store state values (That is JSON response data) here */
+    this.state = this.getTime();
+  }
 
+  componentDidMount() {
+    /* Fetch data here and save it in state */
+    this.setTimer();
+  }
+
+  setTimer() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(this.updateClock.bind(this), 1000);
+  }
+
+  updateClock() {
+    this.setState(this.getTime, this.setTimer);
+  }
+
+  getTime() {
+    const currentTime = new Date();
+
+    return {
+      hours : currentTime.getHours(),
+      minutes : currentTime.getMinutes(),
+      seconds : currentTime.getSeconds(),
+      ampm : currentTime.getHours() >= 12 ? 'pm' : 'am'
+    }
+  }
+
+  render() {
+    const {hours, minutes, seconds, ampm} = this.state;
+
+    return (
+      <div className="App-header">
+        {hours == 0 ? 12 : hours > 12 ? hours - 12 : hours}:
+        {minutes > 9 ? minutes : '0${minutes}'}:
+        {seconds > 9 ? seconds : '0${seconds}'} {ampm}
+      </div>
+    );
+  }
+}
+              
 export default App;
+
+
