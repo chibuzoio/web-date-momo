57c648d87d73602b9854b34001e43dfc26bf1b77
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2023-01-09 01:58:38 +0200

    Added @media properties to all the required css files

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends React.Component {
     return (
       <div>
         {/* <HomePage /> */}
-        <ProfileEditor />
+        <Login />
         {/*<Messenger /> */}
       </div>
     );
