79ad81a1057940ae183d8cd5268e7e2e1545ac00
Chibuzo Onyeokoro <onyeokorochibuzoio@gmail.com>
2023-01-18 07:59:01 +0200

    Tested how to store data in html element and retrieve it in JavaScript

--- src/widget/timeline.js
+++ src/widget/timeline.js
@@ -10,6 +10,10 @@ import RightIconFormField from '../component/right_icon_form_field';
 import icon_heart_hollow from '../image/icon_heart_hollow.png';
 import icon_heart_red from '../image/icon_heart_red.png';
 import icon_search from '../image/icon_search.png';
+import icon_message_blue from '../image/icon_message_blue.png';
+import SexualityOptions from '../widget/sexuality_options';
+import LeftIconHollowButton from '../component/left_icon_hollow_button';
+import icon_view_blue from '../image/icon_view_blue.png';
 import test_image from '../image/test_image.png';
 import logo from '../image/datemomo.png';
 
@@ -37,9 +41,11 @@ class Timeline extends React.Component {
 		super(props);
 		// localStorage.setItem("currentUser", "{}");
 		this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
+		this.displayFloatingLayout = this.displayFloatingLayout.bind(this);
 		this.updateGradientHeight = this.updateGradientHeight.bind(this);
 		this.setGradientHeight = this.setGradientHeight.bind(this);
 		this.changeLikedIcon = this.changeLikedIcon.bind(this);
+		this.getDataValue = this.getDataValue.bind(this);
 	}
 
 	componentDidMount() {
@@ -148,22 +154,99 @@ class Timeline extends React.Component {
 		}
 	}
 
+	displayFloatingLayout(event) {     
+		// Always use event.currentTarget instead of event.target for safe DOM manipulation         
+		console.log("The value of data-current-user on this target is " 
+			+ event.currentTarget.getAttribute("data-current-user"));
+	}
+
+	getDataValue(event) {
+		console.log("The value of data-current-user for userNameLayout here is " 
+			+ event.currentTarget.getAttribute("data-current-user"));
+	}
+
 	render() {        
+		var viewProfileButton = {
+			buttonTitle : "View Profile",
+			buttonIcon : icon_view_blue,
+			leftIconHollowButtonClass : "leftIconHollowButton hollowButton buttonTopMargin " + 
+				"greyHollowButton floatingAccountButton",
+			leftHollowButtonContentClass : "leftHollowButtonContent",
+			hollowButtonLeftIconClass : "hollowButtonLeftIcon",
+			leftHollowButtonTitleClass : "leftHollowButtonTitle"
+		}
+
+		var messageButton = {
+			buttonTitle : "Message",
+			buttonIcon : icon_message_blue,
+			leftIconHollowButtonClass : "leftIconHollowButton hollowButton buttonTopMargin " + 
+				"floatContentRight greyHollowButton floatingAccountButton",
+			leftHollowButtonContentClass : "leftHollowButtonContent",
+			hollowButtonLeftIconClass : "hollowButtonLeftIcon",
+			leftHollowButtonTitleClass : "leftHollowButtonTitle"
+		}
+
+		var sexualCategoryButtons = [
+			{buttonTitle : "Bisexual", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Gay", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Lesbian", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Straight", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sugar Daddy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sugar Mommy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Toy Boy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Toy Girl", buttonClass : "basicButton sexualityButton"}
+		];
+
+		var sexualInterestButtons = [
+			{buttonTitle : "Bisexual", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Friendship", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Gay", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Lesbian", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Relationship", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Straight", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sugar Daddy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sugar Mommy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Toy Boy", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Toy Girl", buttonClass : "basicButton sexualityButton"}
+		];
+    
+		var sexualExperienceButtons = [
+			{buttonTitle : "69", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Anal Sex", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Given Head", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Missionary", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "One-night Stand", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Orgy Sex", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Pool Sex", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Received Head", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sexed In Car", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sexed In Public", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Sexed With Camera", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Threesome", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Used Sex Toys", buttonClass : "basicButton sexualityButton"},
+			{buttonTitle : "Video Sex Chat", buttonClass : "basicButton sexualityButton"}
+		];
+     
 		return (
 			<div>
 				<div className="scrollView">
 					{ 
 						this.state.contextData.userComposite.homeDisplayResponses.map((homeDisplayUser) => ( 
 							<div className="timelineWidget"> 
-								<img className="centerCropped" src={"http://datemomo.com/client/image/" 
+								<img className="centerCropped" onClick={this.displayFloatingLayout} 
+									data-current-user={homeDisplayUser.userName} src={"http://datemomo.com/client/image/" 
 									+ homeDisplayUser.userPictureResponses[0].imageName} />
 								<div className="bottomContentLayout">
-									<div className="userNameLayout">
-										<div className="userNameText">{homeDisplayUser.userName.charAt(0).toUpperCase() 
-											+ homeDisplayUser.userName.slice(1)}, {homeDisplayUser.age}</div>
+									<div className="userNameLayout" data-current-user={homeDisplayUser.memberId}  
+										onClick={this.getDataValue}>
+										<div className="userNameText">
+											{homeDisplayUser.userName.charAt(0).toUpperCase() 
+											+ homeDisplayUser.userName.slice(1)}, {homeDisplayUser.age}
+										</div>
 										<div className="locationText">{homeDisplayUser.currentLocation}</div>
 									</div>
-									<div className="likeIconLayout">
+									<div className="likeIconLayout" ref={(userTimelineLiker) => 
+										{this.userTimelineLiker = userTimelineLiker}}>
 										{this.changeLikedIcon(homeDisplayUser.liked)}
 									</div>
 								</div>
@@ -177,25 +260,32 @@ class Timeline extends React.Component {
 					<div className="floatingUserAccountWidget">
 						<img className="floatingUserAccountImage" 
 							ref={(userAccountImage) => {this.userAccountImage = userAccountImage}} 
-							onLoad={this.setGradientHeight} alt="" src={test_image} />
+							onLoad={this.setGradientHeight} alt="" src={"http://datemomo.com/client/image/" 
+							+ this.state.contextData.floatingAccountData.accountProfilePicture} />
 						<div className="gradientCover" ref={(gradientCover) => {this.gradientCover = gradientCover}} 
-							style={{height : this.state.gradientHeight}}>
+							style={{height : this.state.contextData.floatingAccountData.gradientHeight}}>
 							<div className="gradientLayout">
-								<div className="gradientUserName">Solution, 37</div>
+								<div className="gradientUserName">
+								{this.state.contextData.floatingAccountData.accountUserName.charAt(0).toUpperCase() 
+								+ this.state.contextData.floatingAccountData.accountUserName.slice(1)},&nbsp;
+								{this.state.contextData.floatingAccountData.accountAge}</div>
 								<div className="gradientLocation">Minarelikoy</div>
 							</div>
 						</div>
-						<div className="userStatusText">Hello dear! Welcome to my profile!</div>
+						<div className="userStatusText">{this.state.contextData.floatingAccountData.accountUserStatus}</div>
 						<div className="floatingLayoutButtons">
 							<LeftIconHollowButton buttonParts={viewProfileButton} />
 							<LeftIconHollowButton buttonParts={messageButton} />
 						</div>
 						<div className="floatingSexualityLayout">
-							<div className="sexualityHeader">Kelechi sexuality</div>
+							<div className="sexualityHeader">{this.state.contextData.floatingAccountData.accountUserName.charAt(0).toUpperCase() 
+								+ this.state.contextData.floatingAccountData.accountUserName.slice(1)} sexuality</div>
 							<SexualityOptions sexualityButtons={sexualCategoryButtons} />
-							<div className="sexualityHeader">Kelechi is looking for</div>
+							<div className="sexualityHeader">{this.state.contextData.floatingAccountData.accountUserName.charAt(0).toUpperCase() 
+								+ this.state.contextData.floatingAccountData.accountUserName.slice(1)} is looking for</div>
 							<SexualityOptions sexualityButtons={sexualInterestButtons} />
-							<div className="sexualityHeader">Kelechi sexual fantasies</div>
+							<div className="sexualityHeader">{this.state.contextData.floatingAccountData.accountUserName.charAt(0).toUpperCase() 
+								+ this.state.contextData.floatingAccountData.accountUserName.slice(1)} sexual fantasies</div>
 							<SexualityOptions sexualityButtons={sexualExperienceButtons} />
 						</div>
 					</div>
